7de7dfe256cf8b5bec345546d7626adb3ebe3119
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-31 16:03:51 -0400

    created array of questions

--- script.js
+++ script.js
@@ -6,29 +6,82 @@ const createReadMe = require('./markdownTemplate');
 // THEN a high-quality, professional README.md is generated with the title of my
 // project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 const questions = [
-// name
-// desc
-//instructions
-//usage
-//contribution
-//test
-//license
-//username
-//email address
-//
+    {
+    // name
+        type:'input',
+        name:'title',
+        message:'What is the title of your project? (Required)'
 
+    },
+    {
+    // desc
+        type:'input',
+        name:'description',
+        message:"Please give a short description of your project! (Required)"
 
+    },
+    {
+        //instructions
+        type:'input',
+        name:'installation',
+        message:"Please give a short description of your project! (Required)"
 
+    },
+    {
+        //usage 
+        type:'input',
+        name:'usage',
+        message:'How do you use this project? (Required)'
+    
+    },
+    {
+        //constribution
+        type:'input',
+        name:'contribution',
+        message:"How do you contribute to this project?(Required)"
+    },
+    {   
+        //test
+        type:'input',
+        name:'testing',
+        message:'How do you test this project?(Required)'
 
+    },
+    {   
+        //license
+        type:'checkbox',
+        name:'licensing',
+        message:"Please choose a license for your project! (Required)",
+        choices:['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
 
+    },
+    {
+        //github
+        type:'input',
+        name:'github',
+        message:'Please enter your Github username!(Required)',
+    },
+    {
+        //email 
+        type:'input',
+        name:'email',
+        message:'Please enter in your contact email address!(Required)'
 
-
+    }
 ]
+
+
+
+
+
+
+
+
 
 function askQuestions(){
     inquirer.prompt (questions)
     .then(function(answers){
-        writeToFile("README.md",createReadMe(answers))
+        fs.writeFile("README.md",createReadMe(answers))
     });
 }
 askQuestions();
