b54b415229ee6ff29e3d90c9685f287180076ec1
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-31 15:47:32 -0400

    created function to ask questions then to put that information through the readme function and create a file

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const createReadMe = require('./markdownTemplate');
+
+
+// THEN a high-quality, professional README.md is generated with the title of my
+// project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const questions = [
+// name
+// desc
+//instructions
+//usage
+//contribution
+//test
+//license
+//username
+//email address
+//
+
+
+
+
+
+
+
+]
+
+function askQuestions(){
+    inquirer.prompt (questions)
+    .then(function(answers){
+        writeToFile("README.md",createReadMe(answers))
+    });
+}
+askQuestions();
